b67a06380e24f002cc7b3a52e7eda87a20f9ddb8
Josiel Nass <jnass9961yt@gmail.com>
2020-08-09 20:51:38 -0300

    tUDO fEITO

---
+++ Calculadora.xamarin/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Calculadora.xamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        double n1, n2, res; char MathOperator;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            label.Text += ((Button)sender).Text;
+            
+        }
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            n2 = double.Parse(label.Text);
+            switch (MathOperator)
+            {
+                case '+':
+                    res = n1 + n2;
+                    break;
+                case '-':
+                    res = n1 - n2;
+                    break;
+                case '*':
+                    res = n1 * n2;
+                    break;
+                case '/':
+                    res = n1 / n2;
+                    break;
+                
+            }
+            label.Text = string.Empty;
+            label.Text = Convert.ToString(res);
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+                    
+        }
+
+        async private void Button_Clicked_5(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+       
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            label.Text = string.Empty;
+            n1 = n2 = res = 0;
+           
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            try
+            {
+                n1 = double.Parse(label.Text);
+                MathOperator = Convert.ToChar(((Button)sender).Text);
+                label.Text = string.Empty;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Calculadora.xamarin/Page1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculadora.xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int[] test = {
+                    int.Parse(NumberEntry.Text),
+                int.Parse(NumberEntry.Text) * 2,
+                int.Parse(NumberEntry.Text)*3,
+                int.Parse(NumberEntry.Text)*4,
+                int.Parse(NumberEntry.Text)*5,
+                int.Parse(NumberEntry.Text)*6,
+                int.Parse(NumberEntry.Text)*7,
+                int.Parse(NumberEntry.Text)*8,
+                int.Parse(NumberEntry.Text)*9,
+                int.Parse(NumberEntry.Text)*10
+                };
+                string[] itemsModfied =
+                {
+                    NumberEntry.Text + " X 1 = " +test[0].ToString(),
+                    NumberEntry.Text + " X 2 = " +test[1].ToString(),
+                    NumberEntry.Text + " X 3 = " +test[2].ToString(),
+                    NumberEntry.Text + " X 4 = " +test[3].ToString(),
+                    NumberEntry.Text + " X 5 = " +test[4].ToString(),
+                    NumberEntry.Text + " X 6 = " +test[5].ToString(),
+                    NumberEntry.Text + " X 7 = " +test[6].ToString(),
+                    NumberEntry.Text + " X 8 = " +test[7].ToString(),
+                    NumberEntry.Text + " X 9 = " +test[8].ToString(),
+                    NumberEntry.Text + " X 10 = " +test[9].ToString(),
+                };
+                list.ItemsSource = itemsModfied;
+            }
+            catch
+            {
+
+            }
+        }
+
+        async private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
